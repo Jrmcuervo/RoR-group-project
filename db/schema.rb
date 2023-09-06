da43f7bb481dd2632eb241ba3a3b7fcef6f2b5f7
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-09-06 18:20:55 +0300

    Update files

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_204654) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_132808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "measurement_unit"
-    t.decimal "price"
+    t.integer "price"
     t.integer "quantity"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -37,9 +37,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_204654) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.datetime "preparation_time"
-    t.datetime "cooking_time"
-    t.text "decription"
+    t.string "preparation_time"
+    t.string "cooking_time"
+    t.text "description"
     t.boolean "public"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
